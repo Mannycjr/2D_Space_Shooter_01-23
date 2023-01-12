eae5cc6eb10db5787ccc8016ad5d087627f2a448
MannyCJr <manny@mycjr.com>
2023-01-12 02:57:28 -0800

    Player movement method.

--- 2D Space Shooter Jan 2023/Assets/Scripts/Player.cs
+++ 2D Space Shooter Jan 2023/Assets/Scripts/Player.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    
+    [SerializeField] private float _speed = 3.5f;
+
+    public float horizontalInput;
+    public float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +20,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CalculateMovement();
+
     }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        float verticalLimit = 5.5f;
+        float horizontalLimit = 11.0f;
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -verticalLimit, verticalLimit), 0);
+
+        if (transform.position.x >= horizontalLimit)
+        {
+            transform.position = new Vector3(-horizontalLimit, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -horizontalLimit)
+        {
+            transform.position = new Vector3(horizontalLimit, transform.position.y, 0);
+        }
+    }
+
 }
