cfeda18032dabe48186e46fe3111b73c2aa0728a
MannyCJr <manny@mycjr.com>
2023-02-20 23:41:10 -0800

    Spawned enemy into an enemy container game object

--- 2D Space Shooter Jan 2023/Assets/Scripts/SpawnManager.cs
+++ 2D Space Shooter Jan 2023/Assets/Scripts/SpawnManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _enemyPrefab;
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private GameObject _enemyContainer;
     float _yPositionLimit = 6f;
     float _xPositionLimit = 10.0f;
     float _randomX;
@@ -32,7 +32,8 @@ public class SpawnManager : MonoBehaviour
             _randomX = Random.Range(-_xPositionLimit, _xPositionLimit);
             _randomY = Random.Range(0, _yPositionLimit);
             Vector3 spawnPosition = new Vector3(_randomX, _randomY, 0);
-            Instantiate(_enemyPrefab, spawnPosition, Quaternion.identity);
+            GameObject newEnemy = Instantiate(_enemyPrefab, spawnPosition, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
 
             // yield wait for 5 seconds
             yield return new WaitForSeconds(_waitTime);
