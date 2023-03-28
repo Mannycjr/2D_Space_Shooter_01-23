c19d66bda9c6e8e3d9cc7010f3eb706b3b04f515
mannycjr <manny@mycjr.com>
2023-03-27 17:27:26 -0700

    triple shot powerup spawning logic

--- 2D Space Shooter Jan 2023/Assets/Scripts/SpawnManager.cs
+++ 2D Space Shooter Jan 2023/Assets/Scripts/SpawnManager.cs
@@ -6,20 +6,22 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject _enemyPrefab;
     [SerializeField] private GameObject _enemyContainer;
-    [SerializeField] private GameObject _powerups;
+    [SerializeField] private GameObject _powerupPrefab;
+    [SerializeField] private GameObject _powerupsContainer;
 
     float _yPositionLimit = 6f;
     float _xPositionLimit = 9.0f;
     float _randomX;
     float _randomY;
     float _waitTimeEnemy = 5.0f;
-    float _waitTimePowerups = 7.0f;
+    [SerializeField] float _waitTimePowerups = 7.0f;
     private bool _stopSpawning = false;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerupRoutine());
     }
 
     // Update is called once per frame
@@ -46,9 +48,21 @@ public class SpawnManager : MonoBehaviour
 
     IEnumerator SpawnPowerupRoutine()
     {
+        while (_stopSpawning == false)
+        {
+            // spawn every 3-7 seconds
+            _waitTimePowerups = Random.Range(3.0f, 7.0f);
 
+            // Instantiate prowerup prefab
+            _randomX = Random.Range(-_xPositionLimit, _xPositionLimit);
+            _randomY = Random.Range(0, _yPositionLimit);
+            Vector3 spawnPosition = new Vector3(_randomX, _randomY, 0);
 
-        yield return new WaitForSeconds(_waitTimePowerups);
+            GameObject newPowerup = Instantiate(_powerupPrefab, spawnPosition, Quaternion.identity);
+            newPowerup.transform.parent = _powerupsContainer.transform;
+
+            yield return new WaitForSeconds(_waitTimePowerups);
+        }
     }
 
     public void OnPlayerDeath()
