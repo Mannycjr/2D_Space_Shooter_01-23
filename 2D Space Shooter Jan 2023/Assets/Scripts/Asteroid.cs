4944a3976e4b9df5c502fb276a721c70fcf8a0e7
mannycjr <manny@mycjr.com>
2023-06-16 13:44:20 -0700

    Added thruster VFX. Added logic for player hitting asteroid with ship and taking damage

--- 2D Space Shooter Jan 2023/Assets/Scripts/Asteroid.cs
+++ 2D Space Shooter Jan 2023/Assets/Scripts/Asteroid.cs
@@ -8,6 +8,7 @@ public class Asteroid : MonoBehaviour
     [SerializeField] private GameObject _explosionPrefab;
     private GameObject _explosionInstance;
     private SpawnManager _spawnManager;
+    private Player _player;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,13 @@ public class Asteroid : MonoBehaviour
         {
             Debug.LogError("Asteroid::Start. Spawn Manager is NULL.");
         }
+
+        _player = GameObject.Find("Player").GetComponent<Player>();
+        if (_spawnManager == null)
+        {
+            Debug.LogError("Asteroid::Start. Player is NULL.");
+        }
+
     }
 
     // Update is called once per frame
@@ -30,19 +38,30 @@ public class Asteroid : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Hit! Asteroid::OnTriggerEnter: Begin. other.tag=" + other.tag);
-        if (other.tag == "Laser")
+        if (other.tag == "Laser" ) 
         {
             Debug.Log("Asteroid::OnTriggerEnter: if other.tag=" + other.tag);
             Destroy(other.gameObject);
-            _explosionInstance = Instantiate(_explosionPrefab, transform.position, transform.rotation);
-            Destroy(_explosionInstance,2.5f);
+            ExplosionAnim();
 
             _spawnManager.StartSpawning();
     
         }
+        else if (other.tag == "Player")
+        {
+            ExplosionAnim();
+            _player.Damage();
 
-        
-        
+            _spawnManager.StartSpawning();
+        }
+
+
         Destroy(this.gameObject,0.0f);
+    }
+
+    private void ExplosionAnim()
+    {
+        _explosionInstance = Instantiate(_explosionPrefab, transform.position, transform.rotation);
+        Destroy(_explosionInstance, 2.5f);
     }
 }
