8513e1ae8e3dd1ff262223e83c40dc8fe8492aa3
mannycjr <manny@mycjr.com>
2024-01-24 23:39:00 -0800

    background script for seamless scrolling. first buggy try

--- 2D Space Shooter Jan 2023/Assets/Scripts/Background.cs
+++ 2D Space Shooter Jan 2023/Assets/Scripts/Background.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
+    public float _speed = 2.0f;
+    public float _bottomLimitY = -16.01f;
+    public float _topRespawnY = 24.86f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,10 @@ public class Background : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if (transform.position.y < _bottomLimitY)
+        {
+            transform.position = new Vector3(transform.position.x,_topRespawnY,transform.position.z);
+        }
     }
 }
